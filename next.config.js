2ac51faf5e6018467abcee82f41cfd46033f31f5
hamdanaffiliatepilot-786 <hamdan.affiliatepilot@gmail.com>
2026-06-12 12:03:22 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**.cjdropshipping.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'z-cdn-media.chatglm.cn',
-            },
-        ],
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Sab images allow kar diya hai
+      },
+    ],
+  }
 }
 module.exports = nextConfig;
